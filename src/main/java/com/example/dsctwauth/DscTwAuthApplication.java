ad6ab371016042688a0406ac1b01f80d960995db
Jesus Adair Sosa García <jsosa-linko@cibanco.com>
2025-06-18 22:44:16 -0600

    commit of init Repo

---
+++ src/main/java/com/example/dsctwauth/DscTwAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsctwauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscTwAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscTwAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dsctwauth/DscTwAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsctwauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscTwAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
